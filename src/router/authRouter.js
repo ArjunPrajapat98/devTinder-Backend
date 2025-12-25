eb2e76da7cf862e1a2e2cafa37db74e4e4525b39
Arjun <arju.urja360@gmail.com>
2025-12-25 16:59:06 +0530

    auth router file one small change added

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -4,6 +4,6 @@ export const authRouter = express.Router();
 import { signupValidate, userAuth } from '../config/middleware.js';
 import { loginController, logoutController, signupController } from '../controller/authController.js';
 
-authRouter.post('/signup', userAuth, signupValidate, signupController);
+authRouter.post('/signup', signupValidate, signupController);
 authRouter.post("/login", loginController);
 authRouter.post("/logout", userAuth, logoutController);
