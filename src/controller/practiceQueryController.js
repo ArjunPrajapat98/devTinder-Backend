f2733c6aa7e648ef27805e418df7d268ce351e06
Arjun <arju.urja360@gmail.com>
2026-02-02 15:48:53 +0530

    new changes added

--- src/controller/practiceQueryController.js
+++ src/controller/practiceQueryController.js
@@ -40,3 +40,36 @@
 // const result = await Inventory.find({ quantity: { $ne: 10 } })
 // const result = await Inventory.find({ "carrier.fee": { $ne: 3 } })
 // const result = await Inventory.find({ quantity: { $nin: [50, 10] } })
+
+// other Spices collection
+[
+   { saffron: 5, cinnamon: 5, mustard: null },
+   { saffron: 3, cinnamon: null, mustard: 8 },
+   { saffron: null, cinnamon: 3, mustard: 9 },
+   { saffron: 1, cinnamon: 2, mustard: 3 },
+   { saffron: 2, mustard: 5 },
+   { saffron: 3, cinnamon: 2 },
+   { saffron: 4 },
+   { cinnamon: 2, mustard: 4 },
+   { cinnamon: 2 },
+   { mustard: 6 }
+]
+
+// const result = await Spices.find({ saffron: { $exists: true } })
+// const result = await Spices.find({ cinnamon: { $exists: false } })
+
+// Inventory collection
+[
+   { _id: 1, item: { name: "ab", code: "123" }, qty: 15, tags: ["A", "B", "C"] },
+   { _id: 2, item: { name: "cd", code: "123" }, qty: 20, tags: ["B"] },
+   { _id: 3, item: { name: "mn", code: "456" }, qty: 25, tags: ["A", "B"] },
+   { _id: 4, item: { name: "xy", code: "456" }, qty: 30, tags: ["B", "A"] },
+   { _id: 5, item: { name: "mn", code: "000" }, qty: 20, tags: [["A", "B"], "C"] }
+]
+
+const result = await Inventory.find({
+   $and: [
+      { "item.name": "mn" },
+      { qty: { $eq: 25 } }
+   ]
+})
