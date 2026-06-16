c8d165f9ac97ee727e47fd9b896e99cfb6beb449
Arjun Prajapat <arjun.urja360@gmail.com>
2026-06-16 12:24:56 +0530

    first section changes added

--- src/app.js
+++ src/app.js
@@ -1,26 +1,15 @@
 import express from 'express';
-import { connectDB } from './config/database.js';
 
 const app = express();
-import cookieParser from 'cookie-parser';
-import { authRouter } from './router/authRouter.js';
-import { userRouter } from './router/userRouter.js';
-import { profileRouter } from './router/profileRouter.js';
-import { connectionRouter } from './router/requestRouter.js';
 
-app.use(express.json());
-app.use(cookieParser());
+app.use("/test", (req, res) => {
+    res.status(400).send("Hello world")
+})
 
-app.use('/auth', authRouter);
-app.use('/profile', profileRouter);
-app.use('/user', userRouter);
-app.use('/request', connectionRouter)
+app.get('/userlist', (req, res) => {
+    res.status(200).send('This is a user list data')
+})
 
-connectDB().then(() => {
-    console.log('Database connected successfully')
-    app.listen(4000, () => {
-        console.log('Server is working on Port 4000')
-    })
-}).catch((error) => {
-    console.log('Error : Database , Database not connected')
+app.listen(8100, () => {
+    console.log('Node js server started successfully')
 })
--- src/router/authRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-export const authRouter = express.Router();
-import { signupValidate, userAuth } from '../config/middleware.js';
-import { loginController, logoutController, signupController } from '../controller/authController.js';
-
-authRouter.post('/signup', signupValidate, signupController);
-authRouter.post("/login", loginController);
-authRouter.post("/logout", userAuth, logoutController);
