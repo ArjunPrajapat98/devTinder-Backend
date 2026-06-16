de00d17ee5da5be14632ace4f846e7cca89a7c62
Arjun Prajapat <arjun.urja360@gmail.com>
2026-06-16 14:27:53 +0530

    new chanegs added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { adminAuth } from './middleware/middileware';
+import { adminAuth } from './middleware/middileware.js';
 
 const app = express();
 
 app.get('/setting/:id', adminAuth, (req, res) => {
-    res.status(200).send("Error")
+    res.status(200).send("This is a valid token")
+})
+
+app.get('/dashboard', adminAuth, (req, res) => {
+    res.status(200).send('This is a dashboard')
 })
 
 app.listen(3000, () => {
--- src/middleware/middileware.js
+++ src/middleware/middileware.js
@@ -1,4 +1,12 @@
 
 export const adminAuth = (req, res, next) => {
-    next();
+    let token = '123' // client token
+    let storeToken = '123' // stored token
+    let check = (storeToken === token) ? true : false;
+
+    if (!check) {
+        res.status(200).send('Invalid credentials')
+    } else {
+        next();
+    }
 }
