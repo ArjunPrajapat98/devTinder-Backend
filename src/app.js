f35e9f6868f00c38dae974ccc3a82060cdffd507
Arjun Prajapat <arjun.urja360@gmail.com>
2026-06-16 14:46:03 +0530

    new changes added

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 import express from 'express';
 import { adminAuth } from './middleware/middileware.js';
+import { userRouter } from './router/userRouter/userRouter.js';
 
 const app = express();
 
-app.get('/setting/:id', adminAuth, (req, res) => {
-    res.status(200).send("This is a valid token")
-})
-
-app.get('/dashboard', adminAuth, (req, res) => {
-    res.status(200).send('This is a dashboard')
-})
+app.use('/user', userRouter)
 
 app.listen(3000, () => {
     console.log('Server created successfully')
--- src/router/setttingRouter.js
+++
@@ -1,4 +0,0 @@
-export const routeHandlerFunction = (req, res) => {
-    const param = req.params
-    res.status(200).send(param);
-}
